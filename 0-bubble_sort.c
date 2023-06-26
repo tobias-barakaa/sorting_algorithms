40cdead37919e02178e83b452fc7768372ca8c66
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-26 14:15:17 -0700

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,38 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
 
 /**
-*bubble_sort - algorithm function to print bubble-sort
-*@array: array
-*@size: size of the array
-*Return: return void
-*/
-
+ * bubble_sort - Sorts an array of integers in ascending order using Bubble sort
+ * @array: The array to be sorted
+ * @size: The size of the array
+ * Return: return void
+ */
 void bubble_sort(int *array, size_t size)
 {
-     size_t i, j;
-     int new;
-     int ifTrue;
-     
-     if (array == NULL || size < 2)
-     {
-          return;
-          }
-          
-          for (i = 0; i < size - 1; i++)
-          {
-               ifTrue = 0;
-               for (j = 0; j < size - i - 1; j++)
-               {
-                    if (array[j] > array[j + 1])
-                    {
-                         new = array[j];
-                         array[j] = array[j + 1];
-                         array[j + 1] = new;
-                         ifTrue = 1;
-                         print_array(array, size);
-                         }
-                         }
-                         if (ifTrue == 0)
-                         break;
-                         }
-                         }
+    size_t i, j;
+    int curr;
+    int new;
+
+    if (array == NULL || size < 2)
+    {
+        return;
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        new = 0;
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                curr = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = curr;
+                new = 1;
+                print_array(array, size);
+            }
+        }
+        if (!new == 0)
+            break;
+    }
+}
+

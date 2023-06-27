dc82069ea1f2562b92e7daa37e98652feedea6ea
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-27 08:41:14 -0700

    add

--- sort.h
+++ sort.h
@@ -22,6 +22,6 @@ void print_array(const int *array, size_t size);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-
+void selection_sort(int *array, size_t size);
 
 #endif
